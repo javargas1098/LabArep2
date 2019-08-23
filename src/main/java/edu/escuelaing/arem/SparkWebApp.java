ccf564cc33d307dc9ff9fd62f811930e07e7d7cb
Javier Vargas <ja.vargas1098@gmail.com>
2019-08-22 19:15:26 -0500

    'brt'

--- src/main/java/edu/escuelaing/arem/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arem/SparkWebApp.java
@@ -11,11 +11,18 @@ public class SparkWebApp {
 	public static void main(String[] args) {
 		port(getPort());
 
-		get("/index", (req, res) -> "<!DOCTYPE html>\n" + "<html>\n" + "<body>\n" + "\n"
-				+ "<h2>Calculadora de medias y desviaciones estandar</h2>\n" + "\n" + "<form action=\"/resp\">\n"
-				+ "  Lista de números<br>\n" + "  <input type=\"text\" name=\"numeros\" value=\"\">\n" + "  <br><br>\n"
-				+ "  <input type=\"submit\" value=\"Submit\">\n" + "</form> \n" + "\n" + "\n" + "</body>\n"
-				+ "</html>");
+		get("/index", (req, res) -> "<!doctype html>\r\n" + "<html lang=\"en\">\r\n" + "  <head>\r\n"
+				+ "    <!-- Required meta tags -->\r\n" + "    <meta charset=\"utf-8\">\r\n"
+				+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">\r\n"
+				+ "\r\n" + "    <!-- Bootstrap CSS -->\r\n"
+				+ "    <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\" integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\" crossorigin=\"anonymous\">\r\n"
+				+ "\r\n" + "    <title>Hello, world!</title>\r\n" + "  </head>\r\n" + "  <body>\r\n"
+				+ "    <h1>Hello, world!</h1>\r\n" + "\r\n" + "    <!-- Optional JavaScript -->\r\n"
+				+ "    <!-- jQuery first, then Popper.js, then Bootstrap JS -->\r\n"
+				+ "    <script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\"></script>\r\n"
+				+ "    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\" integrity=\"sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1\" crossorigin=\"anonymous\"></script>\r\n"
+				+ "    <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\" integrity=\"sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM\" crossorigin=\"anonymous\"></script>\r\n"
+				+ "  </body>\r\n" + "</html>");
 		get("/resp",
 				(req, res) -> "<!DOCTYPE html>\n" + "<html>\n" + "<body>\n" + "\n" + "<h2>Respuesta</h2>\n" + "\n"
 						+ "<form action=\"/index\">\n" + "  Media:<br>\n" + media(req.queryParams("numeros"))
@@ -28,7 +35,7 @@ public class SparkWebApp {
 	 * Este metodo Calcula la desviacion estandar a un conjunto de datos
 	 * 
 	 * @param LinkedList<float> Son los datos con los que podra calculará la
-	 *        desviación estandar
+	 *                          desviación estandar
 	 * 
 	 * @return float Desviación estandar
 	 * 
